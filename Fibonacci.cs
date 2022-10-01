22e1dad413eacb17f17ac2d6c092ca5ed6d7f4c9
Marvin G <marvin.gorny99@gmail.com>
2022-10-01 13:41:34 +0200

    Fibonacci Algorithm with C#/.NET

---
+++ Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Fibonacci
+{
+    /**
+     * Make some Fibonacci
+     */
+    class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DoFibonacci(20));
+        }
+
+        /**
+         * Fibonacci algorithm based on formula: f(x) = f(x-1) + f(x-2) for x >= 3
+         *
+         * Prints out the result of the fibonacci number for 'x'
+         */
+        private static int DoFibonacci(int x)
+        {
+            return (x == 1 || x == 2) ? 1 : DoFibonacci(x - 1) + DoFibonacci(x - 2);
+        }
+    }
+}
